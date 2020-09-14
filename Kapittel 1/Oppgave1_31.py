e70439b4b2f33873f622658e60adef4922b40348
Mads Opstad Reistadbakk <mareis@MBP.byod.mrfylke.no>
2020-09-14 11:19:54 +0200

    ,

---
+++ Kapittel 1/Oppgave1_31.py
@@ -0,0 +1,10 @@
+rente = 0.032               # 3.2%
+vekstfaktor = 1 + rente     # Siden saldo øker blir vekstfaktor 1+p
+saldo = 15000               # Beløpet på kontoen
+antall_år = 5
+
+print(f'Saldo etter {antall_år} år: {saldo*vekstfaktor**antall_år:.2f} kr.\n')
+
+for i in range(antall_år):
+    saldo = saldo*(1+rente)
+    print(f'Saldo etter {i+1} år: {saldo:.2f} kr.')
