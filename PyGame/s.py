d4e93e7939b2ecc30cd5e0466dffd4b379746ff1
Mads Opstad Reistadbakk <mareis@MBP.byod.mrfylke.no>
2020-10-01 08:04:34 +0200

    ,

---
+++ PyGame/s.py
@@ -0,0 +1,6 @@
+teller = 1
+nevner = 3
+
+brøk = teller/nevner
+
+print(f"Svaret er: {brøk*100:.1f} %.")
---
+++ PyGame/test.py
@@ -0,0 +1,8 @@
+WIDTH = 500
+HEIGHT = 500
+
+def draw():
+    screen.clear()
+
+
+def update():
