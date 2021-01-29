3e4f7c156325dbcc440f7777898b5b8d0b83081d
mareis <madsor@gmail.com>
2021-01-29 08:30:27 +0100

    ,

---
+++ Figurtall/sum_blomst.py
@@ -0,0 +1,6 @@
+sum = 0
+for n in range(1, 51):
+    a_n = 4 * n**2 - 8 * n + 5
+    sum = sum + a_n
+
+print(f'Summen av firkantene i de 50 første figurene er {sum}.')
---
+++ Figurtall/sum_katt.py
@@ -0,0 +1,8 @@
+sum = 0
+for n in range(1, 101):
+    F_n = 3 * n**2 + 4 * n
+    sum = sum + F_n
+
+print(f'Summen av firkantene i de 100 første figurene er {sum}.')
+
+
